7144408fa10dfb762da20a6b77c21cfd1a531e2b
Rashmi Karnik <raskarnik@deloitte.com>
2025-02-20 16:28:56 -0800

    added changes to main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 ## Google Storage Definition 
 #########################################################################
 # Create Infra Bucket
-resource "google_storage_bucket" "bucket" {
+data "google_storage_bucket" "bucket" {
   project     = var.project_id
   name     = var.infra_bucket
   location = var.region
